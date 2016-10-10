feec78668f337188a85c652fcf638061acc97e07
Sammy <barsamms@gmail.com>
2016-10-10 18:24:37 +0300

    Added bower asset file

---
+++ assets/BowerAsset.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class BowerAsset extends AssetBundle
+{
+    public $sourcePath = '@bower';
+
+    public $css = [
+    ];
+
+    public $js = [
+        'jquery.countdown/dist/jquery.countdown.js'
+    ];
+}
