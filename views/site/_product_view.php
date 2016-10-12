ef3b4d607a315093dcf2640058ab0eaacc7e2176
Sammy <barsamms@gmail.com>
2016-10-12 15:52:24 +0300

    Added on mouse over actions

--- views/site/_product_view.php
+++ views/site/_product_view.php
@@ -13,23 +13,41 @@ use yii\helpers\Url;
 $imageObject = $model->getSingleImage();
 $image = $imageObject ? $imageObject->IMAGE_URL : 'http://placehold.it/400/c55/fff';
 
+//caclulate ther percentage discount based oneth retail price and teh bided amount
+$retail = $model->RETAIL_PRICE;
+$bid = $model->PRICE;
+
+//if retail = 100 bid
+
+$discounted = round((($bid * 100) / $retail), 1);
+
+$discount = "- $discounted";
 ?>
+
+
 <div class="col-xs-18 col-sm-4 col-md-3">
     <div class="productbox">
         <div class="imgthumb img-responsive">
-            <?= Html::img($image, ['width' => 400, 'class' => 'img img-responsive', 'alt' => $model->PRODUCT_NAME]); ?>
+            <?= Html::img($image, [
+                'width' => 400,
+                'class' => 'img img-responsive product-image',
+                'alt' => $model->PRODUCT_NAME,
+                'onmouseover'=>'this.src="http://placehold.it/400/AAFCAE/fff"',
+                'onmouseleave'=>'this.src="'.$image.'"',
+            ]); ?>
+        </div>
+        <div class="saleoffrate">
+            <b><?= $discount; ?>%</b>
         </div>
         <div class="caption">
-            <h5>Thumbnail label</h5>
-            <s class="text-muted"><?= $model->RETAIL_PRICE; ?></s> <b class="finalprice"><?= $model->PRICE; ?></b> from <b>Amazon</b>
+            <h5><?= $model->PRODUCT_NAME; ?></h5>
+            <s class="text-muted"><?= $model->RETAIL_PRICE; ?></s> <b class="finalprice"><?= $model->PRICE; ?></b> from
+            <b>Amazon</b>
             <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i
                     class="glyphicon glyphicon-zoom-in"></i></a>
             <p>
-                <button type="button" class="btn btn-success btn-md btn-block">Get Offer</button>
+                <button type="button" class="btn btn-success btn-md">BID NOW</button>
             </p>
-        </div>
-        <div class="saleoffrate">
-            <b>90%</b><br>OFF
         </div>
     </div>
 </div>
