4653873250e6f8dd28b655487bd1491a362ff3c3
fatelord <barsamms@gmail.com>
2016-10-13 20:23:50 +0300

    Added countdown progress bars

--- views/site/_product_view.php
+++ views/site/_product_view.php
@@ -9,6 +9,7 @@
 /* @var $image app\module\products\models\Images */
 use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\widgets\Pjax;
 
 $imageObject = $model->getSingleImage();
 $imageA = $imageObject ? $imageObject->IMAGE_URL : 'http://placehold.it/800/c55/fff';
@@ -26,29 +27,40 @@ $bidStartTime = 10; //initial start time for the bid
 $productID = $model->PRODUCT_ID;
 ?>
 
-    <div class="col-xs-18 col-sm-4 col-md-3">
-        <ul class="price">
-            <li>
-                <?= Html::img($imageA, [
-                    'width' > '200',
-                    'class' => 'img img-thumbnail',
-                    'alt' => $model->PRODUCT_NAME,
-                    //'onmouseover' => 'this.src="' . $imageB . '"',
-                    //'onmouseleave' => 'this.src="' . $imageA . '"',
-                ]); ?>
-            </li>
-            <li>Starting Bid <?= $model->PRICE; ?></li>
-            <li>Shipping</li>
-            <li><?= $bids; ?> Bids</li>
-            <li>
-                <input type="text" id="<?= $productID; ?>" value="<?= $bidStartTime; ?>" style="width: 50px"/>
+<div class="col-xs-18 col-sm-4 col-md-3" id="item_box_<?= $productID; ?>"">
+<ul class="price">
+    <li>
+        <?= Html::img($imageA, [
+            'width' > '200',
+            'id' => 'product_image_' . $productID,
+            'class' => 'img img-thumbnail',
+            'alt' => $model->PRODUCT_NAME,
+            //'onmouseover' => 'this.src="' . $imageB . '"',
+            //'onmouseleave' => 'this.src="' . $imageA . '"',
+        ]); ?>
+    </li>
+    <li>Starting Bid <?= $model->PRICE; ?></li>
+    <li>Shipping</li>
+    <li><?= $bids; ?> Bids</li>
+    <li>
+        <!--<input type="text" id="<?= $productID; ?>" value="<?= $bidStartTime; ?>" style="width: 50px"/>
                 <button id="startProgressTimer<?= $productID; ?>">Do it!</button>
-                <div id="progressTimer<?= $productID; ?>">Progress</div>
-            </li>
-            <li class="grey"><a href="#" class="button">BID NOW</a></li>
-        </ul>
-    </div>
+                <div id="progressTimer<?= $productID; ?>">Progress</div>-->
+        <div id="progressBar<?= $productID; ?>" class="progressBar">
+            <div></div>
+        </div>
+    </li>
+    <li class="grey">
+        <!--<a href="#" class="button">BID NOW</a>-->
+        <!--<?= Html::a("BID NOW", ['NextItem'], ['class' => 'btn btn-md btn-primary']) ?>-->
+        <?= Html::button('BID NOW', ['class' => 'btn btn-primary', 'onclick' => "placeBid($productID);"]) ?>
+    </li>
+    <li id="info<?= $productID; ?>"></li>
+</ul>
+</div>
+<!--
 
+    -->
 <?php
 /*
 $this->registerJs(
@@ -67,4 +79,62 @@ $this->registerJs(
     });
     });'
 );*/
+
+$this->registerJs(
+    "$(\"document\").ready(function(){ 
+        //call the countdown function
+        progress($bidStartTime, $bidStartTime, $('#progressBar$productID'));
+    });"
+);
 ?>
+
+
+<script type="text/javascript">
+    //handle the progress bar here
+    function progress(timeleft, timetotal, $element) {
+        var progressBarWidth = (timeleft * $element.width()) / timetotal;
+        //console.log(progressBarWidth);
+        $element.find('div').animate({width: progressBarWidth}, timeleft == timetotal ? 0 : 1000, 'linear');
+        console.log(timeleft);
+        if (timeleft > 0) {
+            setTimeout(function () {
+                progress(timeleft - 1, timetotal, $element);
+            }, 1000);
+        } else {
+            console.log("it is over");
+            //perfom ajax call on the element
+        }
+    }
+    ;
+
+    function placeBid($product_id) {
+        //do an ajax request
+        $bidStartTime = 5; //countdown for 10 seconds
+        //alert('Bid placed for ' + $product_id);
+        $.ajax({
+            url: 'http://api.joind.in/v2.1/talks/10889',
+            data: {
+                id: $product_id,
+                sku: 'SKU-67676',
+                user_id: 2,
+                format: 'json'
+            },
+            error: function () {
+                $('#info').html('<p>An error has occurred</p>');
+            },
+            dataType: 'jsonp',
+            success: function (data) {
+                var $title = $('<h1>').text(data.talks[0].talk_title);
+                var $description = $('<p>').text(data.talks[0].talk_description);
+                $('#info' + $product_id)
+                    .append($title)
+                    .append($description);
+
+                //call goin once going twice e.t.c
+                clearInterval(null);
+                progress($bidStartTime, $bidStartTime, $('#progressBar'+$product_id));
+            },
+            type: 'GET'
+        });
+    }
+</script>
--- views/site/index.php
+++ views/site/index.php
@@ -7,9 +7,7 @@ use yii\widgets\ListView;
 
 //var_dump($listDataProvider);
 ?>
-<div id="progressBar">
-    <div></div>
-</div>
+
 <?= ListView::widget([
     'dataProvider' => $listDataProvider,
     'options' => [
@@ -21,47 +19,4 @@ use yii\widgets\ListView;
     //'layout' => "{pager}\n{items}\n{summary}",
     'itemView' => '_product_view',
 ]);
-
-$this->registerJs(
-    '$("document").ready(function(){ 
-    function progress(timeleft, timetotal, $element) {
-    var progressBarWidth = (timeleft * $element.width()) / timetotal;
-          //console.log(progressBarWidth);
-    $element.find(\'div\').animate({ width: progressBarWidth }, timeleft == timetotal ? 0 : 1000, \'linear\');
-        console.log(timeleft);
-    if(timeleft > 0) {
-        setTimeout(function() {
-            progress(timeleft - 1, timetotal, $element);
-        }, 1000);
-    }else{
-        console.log("it is over");
-        //perfom ajax call on the element
-    }
-};
-
-//call the countdown function
-progress(10, 10, $(\'#progressBar\'));
-    });'
-);
 ?>
-
-<style>
-    #progressBar {
-        width: 100%;
-        margin: 10px auto;
-        height: 22px;
-        background-color: transparent;
-        border:1px solid red;
-        /*background-image: linear-gradient(to right, #f00439, #f28d0d);*/
-    }
-
-    #progressBar div {
-        height: 100%;
-        text-align: right;
-        line-height: 22px; /* same as #progressBar height if we want text middle aligned */
-        width: 0;
-        background-color: #CBEA00;
-        box-sizing: border-box;
-    }
-</style>
-
