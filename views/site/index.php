02e4eadc417c284cfd1914118eb8781ff3dc3328
Sammy Barasa <sammy.barasa@pristone.net>
2016-10-26 18:53:54 +0300

    Removed direct bower dpendency

--- views/site/index.php
+++ views/site/index.php
@@ -4,7 +4,8 @@
             <a href="http://www.eoction.com/" class="quickview" data-id="18510" "="">
             <div class="labels">
                 <div class="onsale">-67%</div>
-                <div data-link="http://www.eoction.com/cart-2/" class="viewcart  viewcart-18510" title="View Cart"></div>
+                <div data-link="http://www.eoction.com/cart-2/" class="viewcart  viewcart-18510"
+                     title="View Cart"></div>
             </div>
             <div class="inner">
                 <img width="500" height="500" src="http://placehold.it/500/c66/000"></div>
@@ -21,42 +22,57 @@
                 </div>
             </div>
         </div>
-
-        <a class="product-loop-title" href="http://www.eoction.com/product/2-91ct-blue-topaz-black-spinel-necklaces-sterling-silver-w18/"><h3>2.91Ct Blue Topaz &amp; Black Spinel Necklaces Sterling Silver W/18″</h3></a>
+        <h3>2.91Ct Blue Topaz &amp; Black Spinel Necklaces Sterling Silver W/18″</h3>
 
         <span class="price">
-																		<span class="auction-price" data-auction-id="18510" data-bid="" data-status="running">
-																			<span class="current auction">Starting bid:</span>
-																			<span class="woocommerce-Price-amount amount">
-																				<span class="woocommerce-Price-currencySymbol">$</span>20
-																			</span>
-																		</span>
-																	</span>
-        <div>Shipping: $5</div><span class="time-left">Time left</span>
-        <div class="auction-time-countdown hasCountdown" data-time="1475884800" data-auctionid="18510" data-format="HMS"><span class="countdown_row countdown_show3"><span class="countdown_section"><span class="countdown_amount">36</span><br>Hours</span><span class="countdown_section"><span class="countdown_amount">26</span><br>Minutes</span><span class="countdown_section"><span class="countdown_amount">17</span><br>Seconds</span></span></div>0 Bid
+                <span class="auction-price"
+                      data-auction-id="18510" data-bid=""
+                      data-status="running">
+                <span
+                    class="current auction">Starting bid:</span>
+                <span
+                    class="woocommerce-Price-amount amount">
+                <span
+                    class="woocommerce-Price-currencySymbol">$</span>20
+                </span>
+                </span>
+        </span>
+        <div>Shipping: $5</div>
+        <span class="time-left">Time left</span>
+        <div class="auction-time-countdown hasCountdown" data-time="1475884800" data-auctionid="18510"
+             data-format="HMS"><span class="countdown_row countdown_show3"><span class="countdown_section"><span
+                        class="countdown_amount">36</span><br>Hours</span><span class="countdown_section"><span
+                        class="countdown_amount">26</span><br>Minutes</span><span class="countdown_section"><span
+                        class="countdown_amount">17</span><br>Seconds</span></span></div>
+        0 Bid
         <div class="timerwrapper">
             <div class="shrinking"></div>
-        </div><br>
+        </div>
+        <br>
 
         <div class="add-links-wrap">
             <div class="add-links  clearfix">
-                <a rel="nofollow" href="http://www.eoction.com/product/2-91ct-blue-topaz-black-spinel-necklaces-sterling-silver-w18/" data-quantity="1" data-product_id="18510" data-product_sku="I-CSP3019BUTOPBLP-WSL" class="button product_type_auction add_to_cart_button">Bid now</a>
+                <a rel="nofollow"
+                   href="http://www.eoction.com/product/2-91ct-blue-topaz-black-spinel-necklaces-sterling-silver-w18/"
+                   data-quantity="1" data-product_id="18510" data-product_sku="I-CSP3019BUTOPBLP-WSL"
+                   class="button product_type_auction add_to_cart_button">Bid now</a>
                 <div class="yith-wcwl-add-to-wishlist add-to-wishlist-18510">
                     <div class="yith-wcwl-add-button show" style="display:block">
-                        <a href="http://www.eoction.com/?add_to_wishlist=18510" rel="nofollow" data-product-id="18510" data-product-type="auction" class="add_to_wishlist">
+                        <a href="http://www.eoction.com/?add_to_wishlist=18510" rel="nofollow" data-product-id="18510"
+                           data-product-type="auction" class="add_to_wishlist">
                             Add to Wishlist</a>
                         <span class="ajax-loading"></span>
                     </div>
                     <div class="yith-wcwl-wishlistaddedbrowse hide" style="display:none;">
                         <span class="feedback">Product added!</span>
                         <a href="http://www.eoction.com/wishlist/view/" rel="nofollow">
-                            Browse Wishlist	        </a>
+                            Browse Wishlist </a>
                     </div>
 
                     <div class="yith-wcwl-wishlistexistsbrowse hide" style="display:none">
                         <span class="feedback">The product is already in the wishlist!</span>
                         <a href="http://www.eoction.com/wishlist/view/" rel="nofollow">
-                            Browse Wishlist	        </a>
+                            Browse Wishlist </a>
                     </div>
 
                     <div style="clear:both"></div>
