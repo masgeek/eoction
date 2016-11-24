4aa18bf528fe1149c28569b8d6bfdbfa2fff4574
Sammy Barasa <sammy.barasa@pristone.net>
2016-11-24 13:23:47 +0300

    Added live tv controller

---
+++ controllers/VideoController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KRONOS
+ * Date: 11/24/2016
+ * Time: 1:22 PM
+ */
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class VideoController extends Controller
+{
+    public function actionLiveTV()
+    {
+        return 'Coming soon';
+    }
+}
