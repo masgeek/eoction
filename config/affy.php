a5be62ae763eb774c8b349ac4443d76147bdae13
Sammy Barasa <sammy.barasa@pristone.net>
2016-12-05 23:44:20 +0300

    db conn updates and exclusion list

--- config/affy.php
+++ config/affy.php
@@ -2,9 +2,17 @@
 
 return [
     'class' => 'yii\db\Connection',
-    //'dsn' => 'mysql:host=127.0.0.1;port=3308;dbname=affy',
+    'dsn' => 'mysql:host=127.0.0.1;port=3308;dbname=affy',
+    'username' => 'root',
+    'password' => 'Mj!@#$%^',
+    'charset' => 'utf8',
+];
+
+/*return [
+    'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=104.236.42.19;dbname=app1dbslave',
     'username' => 'root',
     'password' => 'Mj!@#$%^',
     'charset' => 'utf8',
 ];
+*/
--- config/eoction.php
+++ config/eoction.php
@@ -1,18 +1,18 @@
 <?php
-
-/*return [
+return [
     'class' => 'yii\db\Connection',
     //'dsn' => 'mysql:host=localhost;dbname=eoction',
     'dsn' => 'mysql:host=127.0.0.1;dbname=eoction',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
-];*/
+];
 
+/*
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=mysql.eoction.com;dbname=neoct',
     'username' => 'neoctu',
     'password' => '12Hsj&*afh',
     'charset' => 'utf8',
-];
+];*/
