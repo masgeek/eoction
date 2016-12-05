b2adc41da818ec71f3f7b2c36205e5bf671b5a24
Sammy Barasa <sammy.barasa@pristone.net>
2016-12-05 23:37:35 +0300

    db conn update

--- config/affy.php
+++ config/affy.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;port=3308;dbname=affy',
+    //'dsn' => 'mysql:host=127.0.0.1;port=3308;dbname=affy',
+    'dsn' => 'mysql:host=104.236.42.19;dbname=app1dbslave',
     'username' => 'root',
-    'password' => 'sammy',
+    'password' => 'Mj!@#$%^',
     'charset' => 'utf8',
 ];
