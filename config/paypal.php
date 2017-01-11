4af7b2027a0c9b999189a2fcc8c8b6558b373200
Sammy Barasa <sammy.barasa@pristone.net>
2017-01-11 15:23:35 +0300

    updated to paypal live

--- config/paypal.php
+++ config/paypal.php
@@ -10,13 +10,18 @@
  */
 return [
     'class' => 'kongoon\yii2\paypal\Paypal',
-    'clientId' => 'AcH9OlDvwD4g5S7fE4U1m-7tee_CiC6mPH_fxFKzgbiknxg8fTt9xH-VjKNuiLwXhDVg5mLmFx_wKhlS',
-    'clientSecret' => 'ECucZFofNjTKFaTBKqaFHLJxwTGtLfm9dP-s5LIE64IVVNucnwX9qqhfbc6ILyGcI0-WZFJhriThIBtL',
+	/* Test credentials*/
+    //'clientId' => 'AcH9OlDvwD4g5S7fE4U1m-7tee_CiC6mPH_fxFKzgbiknxg8fTt9xH-VjKNuiLwXhDVg5mLmFx_wKhlS',
+    //'clientSecret' => 'ECucZFofNjTKFaTBKqaFHLJxwTGtLfm9dP-s5LIE64IVVNucnwX9qqhfbc6ILyGcI0-WZFJhriThIBtL',
+
+	/* Live credentials */
+	'clientId' => 'AbQ6qNiZGyARxRd7fdzxYAQv2n-S4fOd_zUJNvPV9TwgyamXj7Fe-616wRbTIOlbrWf804AQmPnO2C1N',
+    'clientSecret' => 'ELmHPLvIY5DDnUYIOgkSXHGOvGI3cYmkW-JTaylYemR8RT9Dg2WYlCegeleClZrz3MvL7cARXMNAh2Rm',
     'currency' => 'USD',
-    'isProduction' => false,
+    'isProduction' => true,
     // This is config file for the PayPal system
     'config' => [
-        'http.ConnectionTimeOut' => 30,
+        'http.ConnectionTimeOut' => 60, //script timeout in seconds
         'http.Retry' => 2, //retry only once
         'mode' => \kongoon\yii2\paypal\Paypal::MODE_SANDBOX,    // sandbox | live
         'log.LogEnabled' => YII_DEBUG ? 1 : 0, //based on our environment logs will b enabled or not
