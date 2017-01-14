e150d9b669c9fd4476f8640de3951ee877bb1c64
Sammy Barasa <sammy.barasa@pristone.net>
2017-01-15 00:21:16 +0300

    product count correction

--- components/ProductManager.php
+++ components/ProductManager.php
@@ -108,7 +108,7 @@ class ProductManager
         $item_provider = new ActiveDataProvider([
             'query' => $query, //randomly pick items
             'pagination' => [
-                'pageSize' => $no_of_items = 12
+                'pageSize' => $no_of_items
             ],
         ]);
 
