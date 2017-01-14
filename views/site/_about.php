c75523a825c8ee8eb4abc10a35586de7a4a1e895
Sammy Barasa <sammy.barasa@pristone.net>
2017-01-15 02:53:14 +0300

    Added about page

---
+++ views/site/_about.php
@@ -0,0 +1,6 @@
+<h2><strong>*Overview:*</strong></h2>
+<p>Welcome to eOction where you can shop, save and save even more!</p>
+<h2><strong>*About Us:*</strong></h2>
+<p>eOction is the fastest access point for product purchase and delivery to your home from any device. eOction offers wholesale prices connecting buyers and sellers in real-time Live auctions where any item can sale within 3 minutes. We created an environment where every buyer can request any product with the opportunity to always purchase at the lowest auction price including having the convenience to purchase online and through our Live TV platform.</p>
+<p>We have a solid foundation that comes with over 20 years selling our products to stores such as Macy&rsquo;s, Zale&rsquo;s Jewelry including eBay and Amazon. The same products that you can now buy from eOction at a wholesale cost and save even more through our ecommerce platforms. Our purpose is to always keep our customers in mind and deliver what you want, when you want at the best price.</p>
+<p>We are based in Houston, with offices and warehouses in Mexico, India and China.</p>
