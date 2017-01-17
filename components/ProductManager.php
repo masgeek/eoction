cdf5788c58ec42545993110becde1a51dc7dc627
Sammy Barasa <sammy.barasa@pristone.net>
2017-01-17 14:20:07 +0300

    Updated image checker to ensure if an image does not exists a place holder is shown in its stead

--- components/ProductManager.php
+++ components/ProductManager.php
@@ -299,7 +299,7 @@ class ProductManager
 
     public static function CheckImageExists($image_url)
     {
-        $product_image = 'http://lorempixel.com/460/250/';//'@web/product_images/placeholder.png';
+        $product_image = '@web/product_images/placeholder.png';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $image_url);
         // don't download content
