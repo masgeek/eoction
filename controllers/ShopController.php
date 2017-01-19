65dd79bac9d9d094e853242fa1ffad469ff276a6
Sammy Barasa <sammy.barasa@pristone.net>
2017-01-19 17:25:37 +0300

    relocated shop page to actual shop folder

--- controllers/ShopController.php
+++ controllers/ShopController.php
@@ -54,7 +54,7 @@ class ShopController extends Controller
 	{
 		$dataProvider = ProductManager::GetItemsForSale($no_of_items = 20, $for_auction = [1, 0], $min_stock = 1, $exclusion_list = [], $random = false);
 		$this->view->title = 'Online Shopping';
-		return $this->render('//site/shop', ['listDataProvider' => $dataProvider]);
+		return $this->render('shop', ['listDataProvider' => $dataProvider]);
 	}
 
 	/**
