f2d07505ade6f989b9a3f048b5d0a1ace76d8af2
Sammy Barasa <sammy.barasa@pristone.net>
2017-01-24 19:52:40 +0300

    Added bid request page

---
+++ controllers/RequestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\controllers;
+
+use app\components\ProductManager;
+
+class RequestController extends \yii\web\Controller
+{
+    public $layout = 'column1';
+
+    /**
+     * @return string
+     */
+    public function actionBidRequest()
+    {
+        $dataProvider = ProductManager::GetItemsForSale($no_of_items = 20, $for_auction = [1, 0], $min_stock = 1, $exclusion_list = [], $random = false);
+        $this->view->title = 'Request to Bid';
+
+        return $this->render('bid-request', ['listDataProvider' => $dataProvider]);
+    }
+}
