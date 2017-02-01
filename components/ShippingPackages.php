924c43639ea34eca5577ee43800fa09984857836
Sammy Barasa <sammy.barasa@pristone.net>
2017-02-01 15:32:04 +0300

    retained only three package types

--- components/ShippingPackages.php
+++ components/ShippingPackages.php
@@ -18,7 +18,7 @@ class ShippingPackages
 	CONST USPS_PRIORITY_MAIL_EXPRESS = 'usps_priority_mail_express';
 
 	CONST USPS_FIRST_CLASS_MAIL_INTERNATIONAL = 'usps_first_class_mail_international';
-	CONST USPS_PRIORITY_MAIL_INTERNATIONAL = 'usps_priority_mail_international';
-	CONST USPS_PRIORITY_MAIL_EXPRESS_INTERNATIONAL = 'usps_priority_mail_express_international';
+	//CONST USPS_PRIORITY_MAIL_INTERNATIONAL = 'usps_priority_mail_international';
+	//CONST USPS_PRIORITY_MAIL_EXPRESS_INTERNATIONAL = 'usps_priority_mail_express_international';
 
 }
