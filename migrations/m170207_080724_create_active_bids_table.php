4bea622f70b59374296a7e1c1a0cd3d088ec70d6
Sammy Barasa <sammy.barasa@pristone.net>
2017-02-07 11:17:34 +0300

    Added active bids tracking table Reverted back to bids exclusion table

---
+++ migrations/m170207_080724_create_active_bids_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `active_bids`.
+ */
+class m170207_080724_create_active_bids_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+	    $this->createTable('active_bids', [
+		    'ACTIVE_ID' => $this->primaryKey(),
+		    'PRODUCT_ID' => $this->integer()->unique()->notNull(),
+		    'BIDDING_DURATION' => $this->integer()->notNull()->comment('How long the item'),
+		    'HIGH_DEMAND' => $this->integer(1)->defaultValue(0)->comment('If item is high demand do not exclude it'),
+		    'DATE_CREATED' => $this->timestamp(),
+		    'DATE_UPDATED' => $this->timestamp()
+	    ]);
+
+	    $this->addForeignKey('FK_PRODUCT_ID_ECL', 'bid_exclusion', 'PRODUCT_ID', 'fry_products', 'productid', 'RESTRICT', 'CASCADE');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropTable('active_bids');
+    }
+}
