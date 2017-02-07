15596517a328b10758056e5ffc63fae48ba3e4de
Sammy Barasa <sammy.barasa@pristone.net>
2017-02-07 12:39:27 +0300

    reverted bid exclusion table code

--- migrations/m170207_093804_create_bid_exclusion_table.php
+++ migrations/m170207_093804_create_bid_exclusion_table.php
@@ -10,17 +10,27 @@ class m170207_093804_create_bid_exclusion_table extends Migration
     /**
      * @inheritdoc
      */
-    public function up()
+    public function safeUp()
     {
-        $this->createTable('bid_exclusion', [
-            'id' => $this->primaryKey(),
-        ]);
+	    $this->createTable('bid_exclusion', [
+		    'EXCLUSION_ID' => $this->primaryKey(),
+		    'PRODUCT_ID' => $this->integer()->unique()->notNull(),
+		    'BIDDING_PERIOD' => $this->integer()->notNull()->comment('How long to include the item from bid (hours)'),
+		    'EXCLUSION_PERIOD' => $this->integer()->notNull()->comment('How long to exclude the item from bid (hours)'),
+		    'HIGH_DEMAND' => $this->integer(1)->defaultValue(0)->comment('If item is high demand do not exclude it'),
+		    'DATE_CREATED' => $this->timestamp(),
+		    'DATE_UPDATED' => $this->timestamp()
+	    ]);
+
+	    $this->addForeignKey('FK_PRODUCT_ID_ECL', 'bid_exclusion', 'PRODUCT_ID', 'fry_products', 'productid', 'RESTRICT', 'CASCADE');
+
+
     }
 
     /**
      * @inheritdoc
      */
-    public function down()
+    public function safeDown()
     {
         $this->dropTable('bid_exclusion');
     }
