4d07c37b7fd262cb268a3c20f930a2dad6ff47b2
Sammy Barasa <sammy.barasa@pristone.net>
2017-02-16 14:58:36 +0300

    Added bid requests component

--- bidding/BidRequests.php
+++ bidding/BidRequests.php
@@ -9,9 +9,45 @@
 namespace app\bidding;
 
 
-
-
 class BidRequests extends \yii\base\Component
 {
+    public $timezone;
+    public $current_date;
 
+    public function init()
+    {
+        parent::init();
+
+        if ($this->timezone == null) {
+            $this->timezone = 'GMT';
+        }
+
+        if ($this->current_date == null) {
+            $this->current_date = date('Y-m-d  H:i:s');
+        }
+
+        //set the current timezone
+        date_default_timezone_set($this->timezone);
+    }
+
+    public function ProcessRequests($approved, $request_id = null)
+    {
+        //$this->ProcessProcess($request_id, $approved ? 1 : 0);
+        return $this->Process($approved, $request_id);
+    }
+
+    /**
+     * @param $approved
+     * @param $request_id
+     * @return mixed
+     */
+    private function Process($approved, $request_id)
+    {
+        if ($request_id == null) {
+            //process everything
+        } else {
+            //process the specific request
+        }
+        return $approved;
+    }
 }
