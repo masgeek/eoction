14e21152e1adb922d5b873868b567c6d01279248
Sammy Barasa <sammy.barasa@pristone.net>
2017-02-16 15:15:12 +0300

    Migration class renaming

---
+++ migrations/m170216_121025_messages_init.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+class m170216_121025_messages_init extends Migration
+{
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+        $this->createTable('messages', [
+            'REQUESTER_ID' => $this->primaryKey(),
+            'RECIPIENT' => $this->string(40)->notNull()->comment('Recipient could be phone or email'),
+            'MESSAGE' => $this->text()->comment('Comments'),
+            'CHANNEL' => $this->string(5)->defaultValue(0)->comment('Could be SMS, EMAIL orPUSH'),
+            'MESSAGE_SENT' => $this->boolean()->defaultValue(0)->comment('Request Accepted'),
+            'CREATED' => $this->dateTime()->comment('Date Added'),
+            'UPDATED' => $this->dateTime()->comment('Date Sent'),
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('messages');
+    }
+}
