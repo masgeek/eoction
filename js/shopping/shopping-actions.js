5733b861b0b80dcd365b051a9b4cfebc5fb947ba
Sammy Barasa <sammy.barasa@pristone.net>
2017-02-22 15:05:47 +0300

    Added eslint confguration

--- js/shopping/shopping-actions.js
+++ js/shopping/shopping-actions.js
@@ -1,8 +1,9 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-console,no-undef,no-unused-vars */
 
-function evaluateProducts(product_id){
 
-	$spinnerValue = $('#quantity-'+product_id);
+function evaluateProducts(product_id) {
 
-	console.log('Action started for '+product_id);
+    var $spinnerValue = $('#quantity-' + product_id).val();
+
+    console.log('Action started for ' + $spinnerValue);
 }
