b78d5a19e91219a88509a1c0b31b1f37689aaf63
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-02 14:20:15 +0300

    Added eoction exported database

--- config/eoction.php
+++ config/eoction.php
@@ -3,8 +3,8 @@ return [
     'class' => 'yii\db\Connection',
     //'dsn' => 'mysql:host=localhost;dbname=eoction',
     'dsn' => 'mysql:host=127.0.0.1;dbname=eoction',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => 'eoction',
+    'password' => 'eoction',
     'charset' => 'utf8',
 ];
 
