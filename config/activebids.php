3198fdeee66b3ad7a2605d7634b648bb4c536d6a
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-07 19:02:39 +0300

    Removed recursive loop that caused infinite function loop

--- config/activebids.php
+++ config/activebids.php
@@ -1,6 +1,6 @@
 <?php
 return [
 	'class' => 'app\bidding\ActiveBids',
-	'maximum_items' => YII_DEBUG ? 10 : 24,
+	'maximum_items' => YII_DEBUG ? 10 : 80,
 	'bidding_minute_duration' => YII_DEBUG ? 1 : 5
 ];
